2766aa800bc1dfe40ccca72ac734e48b3729d574
Jesse Frey <jmfrey@alaska.edu>
2013-08-16 11:43:32 -0800

    Simplified the CTL error handler
    Signed-off-by: Jesse Frey <jmfrey@alaska.edu>

--- error.c
+++ error.c
@@ -8,22 +8,8 @@
 
 //something went seriously wrong
 //perhaps should try to recover/log error
-void ctl_handle_error(CTL_ERROR_CODE_t e){
-  switch(e){
-    case CTL_ERROR_NO_TASKS_TO_RUN: 
-      reset(ERR_LEV_CRITICAL,BUS_ERR_SRC_CTL,CTL_ERR_HANDLER,e);
-    break;
-    case CTL_UNSUPPORTED_CALL_FROM_ISR: 
-      reset(ERR_LEV_CRITICAL,BUS_ERR_SRC_CTL,CTL_ERR_HANDLER,e);
-    break;
-    case CTL_UNSPECIFIED_ERROR:
-      reset(ERR_LEV_CRITICAL,BUS_ERR_SRC_CTL,CTL_ERR_HANDLER,e);
-    break;
-    case CTL_MUTEX_UNLOCK_CALL_ERROR:
-      reset(ERR_LEV_CRITICAL,BUS_ERR_SRC_CTL,CTL_ERR_HANDLER,e);
-    break;
-    default:
-      reset(ERR_LEV_CRITICAL,BUS_ERR_SRC_CTL,CTL_ERR_HANDLER,e);
-  }
+void ctl_handle_error(CTL_ERROR_CODE_t e) __toplevel{
+  //a really bad error occurred, reset and report the error 
+  reset(ERR_LEV_CRITICAL,BUS_ERR_SRC_CTL,CTL_ERR_HANDLER,e);
 }
 
