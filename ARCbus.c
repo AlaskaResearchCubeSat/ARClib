fada30e6a980b30a9b31870b479019756601e248
Jesse Frey <jmfrey@alaska.edu>
2015-11-19 09:07:17 -0900

    Increase start timeout to 204

--- ARCbus.c
+++ ARCbus.c
@@ -174,7 +174,7 @@ int BUS_cmd_tx(unsigned char addr,void *buff,unsigned short len,unsigned short f
     return RET_SUCCESS;
   }
   //wait for packet to start
-  e=ctl_events_wait(CTL_EVENT_WAIT_ANY_EVENTS_WITH_AUTO_CLEAR,&arcBus_stat.events,BUS_EV_I2C_MASTER_START,CTL_TIMEOUT_DELAY,104);
+  e=ctl_events_wait(CTL_EVENT_WAIT_ANY_EVENTS_WITH_AUTO_CLEAR,&arcBus_stat.events,BUS_EV_I2C_MASTER_START,CTL_TIMEOUT_DELAY,204);
   //check to see if there was a problem
   if(!(e&BUS_EV_I2C_MASTER_STARTED)){
     //release I2C bus
